c6b3e7b2a71910d96074c5a52f70b09c99226dfe
Jago <126805826+Jagod22@users.noreply.github.com>
2024-09-30 01:35:54 +0300

    Update get_v2.lua

--- getters/get_v2.lua
+++ getters/get_v2.lua
@@ -1,10 +1,11 @@
 local camera = workspace.CurrentCamera
 
 getgenv().get_v2 = function(v3)
+    if typeof(v3) ~= "Vector3" then
+        error("Expected a Vector3 value as input.")
+    end
+    
     local point, onscreen = camera:WorldToViewportPoint(v3)
-    return Vector2.new(
-        point.X,
-        point.Y
-    ),
-    onscreen
+    
+    return Vector2.new(point.X, point.Y), onscreen
 end
