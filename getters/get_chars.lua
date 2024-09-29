24163938525e380912deb115a7a2450ffa23cce8
Jago <126805826+Jagod22@users.noreply.github.com>
2024-09-30 01:33:40 +0300

    Update get_chars.lua

--- getters/get_chars.lua
+++ getters/get_chars.lua
@@ -1,15 +1,15 @@
 local characters
-
-for _, v in next, getgc(true) do
-    if type(v) == "table" then
-        if rawget(v, "getbodyparts") then
-            characters = debug.getupvalue(v.getbodyparts, 1)
+for _, v in pairs(getgc(true)) do
+    if type(v) == "table" and rawget(v, "getbodyparts") then
+        characters = debug.getupvalue(v.getbodyparts, 1)
+        if characters then
+            break
         end
     end
 end
 
 if not characters then
-    return error("failed to find characters")
+    error("Failed to find characters")
 end
 
 getgenv().characters = characters
